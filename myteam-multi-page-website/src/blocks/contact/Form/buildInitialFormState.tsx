95a61921fa69604e43d3072b0e7277fc15de3c9b
Guillaume Lorel <guill@LAPTOP-LDN2H8L9>
2024-09-02 22:03:42 +0200

    fix deployment error

--- myteam-multi-page-website/src/blocks/contact/Form/buildInitialFormState.tsx
+++ myteam-multi-page-website/src/blocks/contact/Form/buildInitialFormState.tsx
@@ -2,41 +2,12 @@ import { FormFieldBlock } from "@payloadcms/plugin-form-builder/types";
 
 export const buildInitialFormState = (fields: FormFieldBlock[]) => {
   return fields.reduce((initialSchema, field) => {
-    if (field.blockType === "checkbox") {
-      return {
-        ...initialSchema,
-        [field.name]: false,
-      };
-    }
-    if (field.blockType === "country") {
+    if (field.blockType === "email" || field.blockType === "text") {
       return {
         ...initialSchema,
         [field.name]: "",
       };
     }
-    if (field.blockType === "email") {
-      return {
-        ...initialSchema,
-        [field.name]: "",
-      };
-    }
-    if (field.blockType === "text") {
-      return {
-        ...initialSchema,
-        [field.name]: "",
-      };
-    }
-    if (field.blockType === "select") {
-      return {
-        ...initialSchema,
-        [field.name]: "",
-      };
-    }
-    if (field.blockType === "state") {
-      return {
-        ...initialSchema,
-        [field.name]: "",
-      };
-    }
-  }, {});
+    return initialSchema;
+  }, {} as Record<string, any>);
 };
